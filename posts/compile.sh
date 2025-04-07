033705835785071a1092073f734617223277c8ef
FreePlacki <bieleckipawel10@gmail.com>
2025-04-07 20:14:47 +0200

    Some styles cleanup

--- posts/compile.sh
+++ posts/compile.sh
@@ -1,6 +1,6 @@
 pandoc content.md -s -o content.html --highlight-style=tango --mathml
 
-# add:
+# add (instead of generated <style></style>)
 # <link rel="stylesheet" href="../../custom-highlight.css" />
 # <link rel="stylesheet" href="../../styles.css" />
-
+# <link rel="stylesheet" href="../../post.css" />
